9e9a14af6aaec2a59463318aed01b02828bf567b
natureloved <akinolaa769@gmail.com>
2026-03-16 13:00:44 +0100

    feat: add telegram web app script to dashboard layout

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import Script from "next/script";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Script 
+        src="https://telegram.org/js/telegram-web-app.js" 
+        strategy="beforeInteractive" 
+      />
+      {children}
+    </>
+  );
+}
